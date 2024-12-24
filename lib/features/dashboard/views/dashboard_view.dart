b4e8c6e6e87efa319f95988b70ba6712fa04adaa
MohamedElhosaraty <mohamedelhosaraty05@gmail.com>
2024-12-24 20:34:32 +0200

    create dashboard_view and dashboard_view_body

---
+++ lib/features/dashboard/views/dashboard_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_dashboard/features/dashboard/views/widgets/dashboard_view_body.dart';
+
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
+
+  static const routeName = 'dashboard';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: DashboardViewBody(),
+    );
+  }
+}
---
+++ lib/features/dashboard/views/widgets/dashboard_view_body.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_dashboard/core/widget/custom_button.dart';
+import 'package:fruit_dashboard/features/add_product/presentation/views/add_product_view.dart';
+
+class DashboardViewBody extends StatelessWidget {
+  const DashboardViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal:
+      16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CustomButton(onPressed: (){
+            Navigator.pushNamed(context, AddProductView.routeName);
+          }, text: "Add Data"),
+        ],
+      ),
+    );
+  }
+}
